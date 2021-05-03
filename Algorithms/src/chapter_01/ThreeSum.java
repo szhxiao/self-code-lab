49661655394cf8a13178b31a7d8c600ff8780b59
szhxiao <szhxiao@yeah.net>
2021-05-03 09:09:24 +0800

    Add 1.4 of chapter_01

---
+++ Algorithms/src/chapter_01/ThreeSum.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+/**
+ * Algorithms
+ *
+ * @version 4th
+ * @author Robert Sedgewick
+ *         Kevin Wayne
+ * @see http://algs4.cs.princeton.edu
+ */
+
+public class ThreeSum {
+    public static int count(int[] a) {
+        int N = a.length;
+        int cnt = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = i + 1; j < N; j++) {
+                for (int k = j + 1; k < N; k++) {
+                    if (a[i] + a[j] + a[k] == 0) {
+                        cnt++;
+                    }
+                }
+            }
+        }
+        return cnt;
+    }
+
+    public static void main(String[] args) {
+        // Scanner in = new Scanner(System.in);
+        int[] a = In.readInts(args[0]);
+        System.out.println(count(a));
+    }
+}
+
+/**
+ * output:
+ *
+ */
