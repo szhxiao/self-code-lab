cfbc7bfdcb01f413cb155a21809a39ceae68bfc9
szhxiao <szhxiao@yeah.net>
2022-04-07 17:48:32 +0800

    Add chapter_02_变量和简单数据类型 of PythonCrashCourse

---
+++ PythonCrashCourse/src/chapter_02/float.py
@@ -0,0 +1,12 @@
+# Python Crash Course
+# A Hands-On, Project-Based Introduction to Programming
+#
+# version: 2nd
+# author: Eric Matthes
+# http://ituring.cn/book/2784
+
+number = 0.1 + 0.2
+print(number)
+
+# output:
+# 0.30000000000000004
---
+++ PythonCrashCourse/src/chapter_02/full_name.py
@@ -0,0 +1,12 @@
+# Python Crash Course
+# A Hands-On, Project-Based Introduction to Programming
+#
+# version: 2nd
+# author: Eric Matthes
+# http://ituring.cn/book/2784
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = f"{first_name} {last_name}"
+message = f"Hello, {full_name.title()}"
+print(message)
