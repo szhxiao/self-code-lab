f6fe36f584a58f1bde9e26a42c76a8fc5b364553
szhxiao <szhxiao@yeah.net>
2024-12-22 02:35:17 +0800

    Add chapter-16  下载数据

---
+++ PythonCrashCourse/src/chapter-16/show_color_scales.py
@@ -0,0 +1,11 @@
+# Python Crash Course
+# A Hands-On, Project-Based Introduction to Programming
+#
+# version: 2nd
+# author: Eric Matthes
+# http://ituring.cn/book/2784
+
+import plotly.express as px
+
+for key in px.colors.named_colorscales():
+    print(key)
